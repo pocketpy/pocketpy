53b33eb2f2282c88749e6208fee738ff2272d4c4
blueloveTH <blueloveth@hclcat.games>
2022-11-13 16:04:12 +0800

    add str escape

--- src/str.h
+++ src/str.h
@@ -135,6 +135,34 @@ public:
         }));
         return _Str(copy);
     }
+
+    _Str __escape(bool single_quote) const {
+        _StrStream ss;
+        for (auto c = _s.cbegin(); c != _s.cend(); c++) {
+            switch (*c) {
+                case '"':
+                    if(!single_quote) ss << '\\';
+                    ss << '"';
+                    break;
+                case '\'':
+                    if(single_quote) ss << '\\';
+                    ss << '\'';
+                    break;
+                case '\\': ss << '\\' << '\\'; break;
+                case '\n': ss << "\\n"; break;
+                case '\r': ss << "\\r"; break;
+                case '\t': ss << "\\t"; break;
+                default:
+                    if ('\x00' <= *c && *c <= '\x1f') {
+                        ss << "\\u"
+                        << std::hex << std::setw(4) << std::setfill('0') << static_cast<int>(*c);
+                    } else {
+                        ss << *c;
+                    }
+            }
+        }
+        return ss.str();
+    }
 };
 
 
