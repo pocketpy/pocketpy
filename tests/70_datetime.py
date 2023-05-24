b71e3e1e7453a037494ae3575da3cfdf6f374755
小染 <56108982+XR-stb@users.noreply.github.com>
2023-05-24 14:19:02 +0800

    remove \ 70_datetime.py
    Remove  line continuation character（\）, which is not currently supported

--- tests/70_datetime.py
+++ tests/70_datetime.py
@@ -27,19 +27,15 @@ time.max = time(23, 59, 59, 999999)
 time.resolution = timedelta(microseconds=1)
 
 
-assert '12:34' == \
-       str(time(hour=12, minute=34, second=56, microsecond=123456).isoformat(timespec='minutes'))
+assert '12:34' == str(time(hour=12, minute=34, second=56, microsecond=123456).isoformat(timespec='minutes'))
 
 tm = time(hour=12, minute=34, second=56, microsecond=0)
 assert '12:34:56.000000' == str(tm.isoformat(timespec='microseconds'))
 
 assert '12:34:56' == str(tm.isoformat(timespec='auto'))
 
-assert 'datetime.datetime(2011, 11, 4)' \
-       == str(datetime.fromisoformat('2011-11-04'))
+assert 'datetime.datetime(2011, 11, 4)' == str(datetime.fromisoformat('2011-11-04'))
 
-assert 'datetime.datetime(2011, 11, 4)' \
-       == str(datetime.fromisoformat('20111104'))
+assert 'datetime.datetime(2011, 11, 4)' == str(datetime.fromisoformat('20111104'))
 
-assert 'datetime.datetime(2011, 11, 12)' \
-       == str(datetime.fromisoformat('2011-11-12'))
+assert 'datetime.datetime(2011, 11, 12)' == str(datetime.fromisoformat('2011-11-12'))
