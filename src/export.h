9fdac89b4bf546b769695d001c5a004c1d4a1a5e
BLUELOVETH <blueloveTH@foxmail.com>
2023-06-24 03:58:47 +0800

    Update export.h

--- src/export.h
+++ src/export.h
@@ -2,15 +2,13 @@
 
 #ifdef _WIN32
 #define PK_EXPORT __declspec(dllexport)
-#elif __APPLE__
-#define PK_EXPORT __attribute__((visibility("default"))) __attribute__((used))
 #elif __EMSCRIPTEN__
 #include <emscripten.h>
 #define PK_EXPORT EMSCRIPTEN_KEEPALIVE
 #else
-#define PK_EXPORT
+#define PK_EXPORT __attribute__((visibility("default"))) __attribute__((used))
 #endif
 
-#define PK_LEGACY_EXPORT PK_EXPORT
+#define PK_LEGACY_EXPORT PK_EXPORT inline
 
 #endif
