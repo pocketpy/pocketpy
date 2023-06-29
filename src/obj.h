d8ea87e4083a492c4e95efff3dcf9837f5e60de4
blueloveTH <blueloveTH@foxmail.com>
2023-06-29 22:38:55 +0800

    ...
    ...

--- src/obj.h
+++ src/obj.h
@@ -306,7 +306,7 @@ __T _py_cast(VM* vm, PyObject* obj) {
 #define _CAST(T, x) _py_cast<T>(vm, x)
 
 #define CAST_F(x) vm->num_to_float(x)
-#define CAST_DEFAULT(T, i, default_value) (args[i] != vm->None) ? py_cast<T>(vm, args[i]) : (default_value)
+#define CAST_DEFAULT(T, x, default_value) (x != vm->None) ? py_cast<T>(vm, x) : (default_value)
 
 /*****************************************************************/
 template<>
