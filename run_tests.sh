fdcf0c8df57015b8b5167b4546680e2620689777
blueloveTH <blueloveTH@foxmail.com>
2023-07-04 21:15:52 +0800

    ...

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,9 @@
-python3 preprocess.py
-clang++ -std=c++17 -fno-rtti --coverage -O1 -stdlib=libc++ -Wall -o pocketpy src/main.cpp
+python3 prebuild.py
+SRC=$(find src/ -name "*.cpp")
+clang++ -std=c++17 -fno-rtti --coverage -O1 -stdlib=libc++ -Wfatal-errors -o pocketpy src2/main.cpp $SRC -Iinclude
 python3 scripts/run_tests.py
 rm -rf .coverage
 mkdir -p .coverage
 llvm-cov-15 gcov main.gc -r -s src/ >> .coverage/coverage.txt
 mv *.gcov .coverage
-rm main.gc*
+rm *.gc*
