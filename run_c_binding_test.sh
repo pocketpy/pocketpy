cf2fe7236d2434db540292d97fe05be8a0dd60f4
BLUELOVETH <blueloveTH@foxmail.com>
2023-07-07 17:41:48 +0800

    ...

--- run_c_binding_test.sh
+++ run_c_binding_test.sh
@@ -1,26 +1,14 @@
-python3 preprocess.py
+cd c_bindings
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
 
-echo "compiling c++ lib"
-clang++ -c -o pocketpy_c.o c_bindings/pocketpy_c.cpp -Wfatal-errors -O1 --std=c++17 -Wall -Wno-sign-compare -Wno-unused-variable -fno-rtti -stdlib=libc++ -I src/ -g
-
-echo "compiling c executable" 
-clang -c -o test.o c_bindings/test.c -Wfatal-errors -Wall -O1 -Wno-sign-compare -Wno-unused-variable -I src/ -g
-echo "linking"
-clang++ -o c_binding_test test.o pocketpy_c.o -stdlib=libc++ -g
-./c_binding_test > binding_test_scratch
+./test_c_bindings > binding_test_scratch
 echo "checking results (they should be identical)"
-diff -q -s  binding_test_scratch c_bindings/test_answers.txt
+diff -q -s  binding_test_scratch ../test_answers.txt
 if [ $? -eq 1 ]
 then
     echo "ERROR: c binding test failed"
-    rm pocketpy_c.o
-    rm test.o
     exit 1
 fi
-
-echo "cleaning up"
-rm pocketpy_c.o
-rm test.o
-rm binding_test_scratch
-rm c_binding_test
-
