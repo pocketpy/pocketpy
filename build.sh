f0a2ea215ed6a3fcbb03d5e9e3b3a878b35e75e4
BLUELOVETH <blueloveTH@foxmail.com>
2023-07-18 17:48:39 +0800

    remove `-fno-rtti`

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 python3 prebuild.py
 SRC=$(find src/ -name "*.cpp")
-FLAGS="-std=c++17 -fno-rtti -O2 -stdlib=libc++ -Wfatal-errors -Iinclude"
+FLAGS="-std=c++17 -O2 -stdlib=libc++ -Wfatal-errors -Iinclude"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     LIB_EXTENSION=".dylib"
     FLAGS="$FLAGS -undefined dynamic_lookup"
+    LINK_FLAGS=""
 else
     LIB_EXTENSION=".so"
+    LINK_FLAGS="-Wl,-rpath=."
 fi
 clang++ $FLAGS -o libpocketpy$LIB_EXTENSION $SRC -fPIC -shared -ldl
 
 # compile main.cpp and link to libpocketpy.so
-clang++ $FLAGS -o main src2/main.cpp -L. -lpocketpy -Wl,-rpath=.
+clang++ $FLAGS -o main src2/main.cpp -L. -lpocketpy $LINK_FLAGS
