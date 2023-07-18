692f3c52665ecf0d6e816976b1b8e7d53d8d7a99
blueloveTH <blueloveTH@foxmail.com>
2023-07-18 22:44:25 +0800

    ...

---
+++ scripts/genstub.py
@@ -0,0 +1,34 @@
+import os
+
+with open('include/pocketpy/pocketpy_c.h') as f:
+    lines = f.readlines()
+
+a = []
+for line in lines:
+    if line.startswith("PK_EXPORT"):
+        _, ret, *body = line.split()
+    else:
+        continue
+    body = ' '.join(body)
+    assert body.endswith(";")
+    body = body[:-1]
+
+    if ret == 'void':
+        mock_string = ''
+    else:
+        mock_string = ' '*4 + ret + ' returnValue;\n    return returnValue;'
+
+    a.append(
+        ret + ' ' + body + ' {\n' + mock_string + '\n}\n'
+    )
+
+with open('src2/pocketpy_c.cpp', 'w') as f:
+    f.write('''
+#include "pocketpy_c.h"
+
+#ifdef _WIN32
+#pragma warning(disable: 4700)
+#endif
+            
+''')
+    f.write('\n'.join(a))
