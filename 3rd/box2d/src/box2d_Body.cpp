719cc7979226a97c565eaa6cf4766b978d20b489
blueloveTH <blueloveTH@foxmail.com>
2023-08-06 18:37:29 +0800

    fix a bug

--- 3rd/box2d/src/box2d_Body.cpp
+++ 3rd/box2d/src/box2d_Body.cpp
@@ -108,7 +108,7 @@ void PyBody::_register(VM* vm, PyObject* mod, PyObject* type){
 
     // get_node
     vm->bind(type, "get_node(self)", [](VM* vm, ArgsView args){
-        PyBody& body = CAST(PyBody&, args[1]);
+        PyBody& body = CAST(PyBody&, args[0]);
         return body.node_like;
     });
 
