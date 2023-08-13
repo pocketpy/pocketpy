4c6369448522e49b56e13094d5502007869a8249
blueloveTH <blueloveTH@foxmail.com>
2023-08-13 17:10:18 +0800

    ...

--- dylib/xmake.lua
+++ dylib/xmake.lua
@@ -1,10 +1,88 @@
+add_rules("mode.debug", "mode.release")
+
 set_languages("c11")
 
-target("pocketpy-shared")
-    set_kind("shared")
-    add_files(path.join(os.projectdir(), "src2", "pocketpy_c.c"))
+root_dir = "../"
 
+-- Include directories
+add_includedirs(root_dir .. "include")
+
+-- Define the shared library target for pocketpy
+target("pocketpy")
+    set_kind("shared")
+    add_files(root_dir .. "src2/pocketpy_c.c")
+
+-- Define the shared library target
 target("test")
     set_kind("shared")
     add_files("src/test.c")
-    add_deps("pocketpy-shared")
+    add_deps("pocketpy")
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro definition
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
