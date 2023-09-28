a3d4e6695850dd0c0114176a2be23635db6322a7
blueloveTH <blueloveth@foxmail.com>
2023-09-28 11:58:24 +0800

    ...

--- include/pocketpy/export.h
+++ include/pocketpy/export.h
@@ -37,7 +37,9 @@
 #define NOMB              // MB_* and MessageBox()
 #define NOMEMMGR          // GMEM_*, LMEM_*, GHND, LHND, associated routines
 #define NOMETAFILE        // typedef METAFILEPICT
-#define NOMINMAX          // Macros min(a,b) and max(a,b)
+#ifndef NOMINMAX
+    #define NOMINMAX          // Macros min(a,b) and max(a,b)
+#endif
 #define NOMSG             // typedef MSG and associated routines
 #define NOOPENFILE        // OpenFile(), OemToAnsi, AnsiToOem, and OF_*
 #define NOSCROLL          // SB_* and scrolling routines
