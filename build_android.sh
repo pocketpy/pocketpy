e77331e57a418105ddf1ba1600b5e90bf89cefac
blueloveTH <blueloveTH@foxmail.com>
2023-12-09 19:53:52 +0800

    ...

--- build_android.sh
+++ build_android.sh
@@ -10,6 +10,7 @@ cmake \
     -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
     -DANDROID_ABI=$1 \
     -DANDROID_PLATFORM=android-22 \
+    -DANDROID_STL=c++_shared \
     ../../.. \
     -DPK_BUILD_SHARED_LIB=ON -DPK_USE_CJSON=ON -DPK_USE_BOX2D=ON \
     -DCMAKE_BUILD_TYPE=Release
