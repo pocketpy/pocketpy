819ef471b72dba9f586715269cd71a5f37f4706d
blueloveTH <blueloveTH@foxmail.com>
2023-12-09 19:01:40 +0800

    update android workflow

---
+++ build_android.sh
@@ -0,0 +1,14 @@
+# if no $1 default arm64-v8a
+if [ -z $1 ]; then
+    $1=arm64-v8a
+fi
+
+mkdir -p build/android/$1
+cd build/android/$1
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI=$1 \
+    -DANDROID_PLATFORM=android-22 \
+    ../../..
+    -DPK_BUILD_SHARED_LIB=ON -DPK_USE_CJSON=ON -DPK_USE_BOX2D=ON
