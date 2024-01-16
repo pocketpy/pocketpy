0ae822f975460d11d2bcdc2a735688d3c8fca515
blueloveTH <blueloveth@foxmail.com>
2024-01-16 18:50:21 +0800

    use `-Os` in `build_web.sh`

--- build_web.sh
+++ build_web.sh
@@ -4,4 +4,4 @@ rm -rf web/lib
 mkdir web/lib
 
 SRC=$(find src/ -name "*.cpp")
-em++ $SRC -Iinclude/ -fexceptions -O3 -sEXPORTED_FUNCTIONS=_pkpy_new_repl,_pkpy_repl_input,_pkpy_new_vm -sEXPORTED_RUNTIME_METHODS=ccall -o web/lib/pocketpy.js
+em++ $SRC -Iinclude/ -fexceptions -Os -sEXPORTED_FUNCTIONS=_pkpy_new_repl,_pkpy_repl_input,_pkpy_new_vm -sEXPORTED_RUNTIME_METHODS=ccall -o web/lib/pocketpy.js
