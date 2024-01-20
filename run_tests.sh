5603a0de1e6d5b2cf72f040f850b9de815e42ba5
blueloveTH <blueloveTH@foxmail.com>
2024-01-20 22:59:54 +0800

    test with `PK_ENABLE_OS=1`

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 python3 prebuild.py
 SRC=$(find src/ -name "*.cpp")
-clang++ -std=c++17 --coverage -O1 -stdlib=libc++ -Wfatal-errors -o main src2/main.cpp $SRC -Iinclude
+clang++ -std=c++17 --coverage -O1 -stdlib=libc++ -Wfatal-errors -o main src2/main.cpp $SRC -Iinclude -DPK_ENABLE_OS=1
 python3 scripts/run_tests.py
 
 # if prev error exit
