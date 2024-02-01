c204e07c9c3084b92f981f3cc289c5470c6b9704
blueloveTH <blueloveth@foxmail.com>
2024-02-01 15:20:34 +0800

    enable test cases in `benchmarks/`

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,9 @@
 python3 prebuild.py
 SRC=$(find src/ -name "*.cpp")
 clang++ -std=c++17 --coverage -O1 -stdlib=libc++ -Wfatal-errors -o main src2/main.cpp $SRC -Iinclude -DPK_ENABLE_OS=1
+
 python3 scripts/run_tests.py
+python3 scripts/run_tests.py benchmarks/
 
 # if prev error exit
 if [ $? -ne 0 ]; then
