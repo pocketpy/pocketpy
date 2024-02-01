703bacc7de487bf9e0a834c101c11ca916c709bc
BLUELOVETH <blueloveTH@foxmail.com>
2024-02-01 18:53:07 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ SRC=$(find src/ -name "*.cpp")
 clang++ -std=c++17 --coverage -O1 -stdlib=libc++ -Wfatal-errors -o main src2/main.cpp $SRC -Iinclude -DPK_ENABLE_OS=1
 
 python3 scripts/run_tests.py
-python3 scripts/run_tests.py benchmarks/
+# python3 scripts/run_tests.py benchmarks/
 
 # if prev error exit
 if [ $? -ne 0 ]; then
