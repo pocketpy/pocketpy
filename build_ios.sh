881fbb01dd381ca133240e83ae2851976c097dbc
blueloveTH <blueloveth@foxmail.com>
2024-03-23 16:51:48 +0800

    some fix

--- build_ios.sh
+++ build_ios.sh
@@ -7,15 +7,12 @@ FLAGS="-DCMAKE_TOOLCHAIN_FILE=~/ios-cmake/ios.toolchain.cmake -DPK_BUILD_STATIC_
 cmake -B os64 -G Xcode $FLAGS -DPLATFORM=OS64 ..
 cmake --build os64 --config Release
 
-cmake -B simulator64 -G Xcode $FLAGS -DPLATFORM=SIMULATOR64 ..
-cmake --build simulator64 --config Release
-
-# cmake -B simulatorarm64 -G Xcode $FLAGS -DPLATFORM=SIMULATORARM64 ..
-# cmake --build simulatorarm64 --config Release
+cmake -B simulatorarm64 -G Xcode $FLAGS -DPLATFORM=SIMULATORARM64 ..
+cmake --build simulatorarm64 --config Release
 
 xcodebuild -create-xcframework \
-    -framework os64/Release-iphoneos/pocketpy.framework \
-    -framework simulator64/Release-iphonesimulator/pocketpy.framework \
+    -library os64/Release-iphoneos/libpocketpy.a -headers ../include \
+    -library simulatorarm64/Release-iphonesimulator/libpocketpy.a -headers ../include \
     -output pocketpy.xcframework
 
 
