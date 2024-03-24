92bc4b5495941e342aea570c2e8eb87479c65981
blueloveTH <blueloveth@foxmail.com>
2024-03-24 20:19:04 +0800

    Update common.h

--- include/pocketpy/common.h
+++ include/pocketpy/common.h
@@ -72,11 +72,11 @@ template <size_t T>
 struct NumberTraits;
 
 inline constexpr bool is_negative_shift_well_defined(){
-#ifdef __EMSRIPTEN__
+#ifdef __EMSCRIPTEN__
 	return false;
 #endif
 	// rshift does not affect the sign bit
-	return ((int)-1) >> 1 == -1 && ((int64_t)-1) >> 1 == -1;
+	return -1 >> 1 == -1;
 }
 
 template <>
