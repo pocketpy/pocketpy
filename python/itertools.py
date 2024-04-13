062da8b8aed84f1077ac190ed8c787588410710f
blueloveTH <blueloveTH@foxmail.com>
2024-04-13 23:43:11 +0800

    add `itertools`

---
+++ python/itertools.py
@@ -0,0 +1,13 @@
+def zip_longest(a, b):
+    a = iter(a)
+    b = iter(b)
+    while True:
+        ai = next(a)
+        bi = next(b)
+        if ai is StopIteration and bi is StopIteration:
+            break
+        if ai is StopIteration:
+            ai = None
+        if bi is StopIteration:
+            bi = None
+        yield ai, bi
