372446c1d25ee851d9fa01d4c9495dd3530f513b
blueloveTH <blueloveth@foxmail.com>
2024-04-24 15:04:55 +0800

    Update build_with_warnings.sh

--- build_with_warnings.sh
+++ build_with_warnings.sh
@@ -1,5 +1,5 @@
 SRC=$(find src/ -name "*.cpp")
 
-FLAGS="-std=c++17 -O1 -stdlib=libc++ -Iinclude -frtti -W -Wno-unused-parameter"
+FLAGS="-std=c++17 -O1 -stdlib=libc++ -Iinclude -frtti -W -Wno-unused-parameter -Wno-sign-compare"
 
 clang++ $FLAGS -o main -O1 src2/main.cpp $SRC
