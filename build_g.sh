049a5fadee5005a93e5b1a14eab7082f7c86c9ea
blueloveTH <blueloveTH@foxmail.com>
2024-05-13 02:28:37 +0800

    some fix

--- build_g.sh
+++ build_g.sh
@@ -1,5 +1,5 @@
 SRC=$(find src/ -name "*.cpp")
 
-FLAGS="-std=c++17 -O1 -stdlib=libc++ -Iinclude -frtti -Wfatal-errors -g"
+FLAGS="-std=c++17 -O0 -stdlib=libc++ -Iinclude -frtti -Wfatal-errors -g"
 
-clang++ $FLAGS -o main -O1 src2/main.cpp $SRC
+clang++ $FLAGS -o main src2/main.cpp $SRC
