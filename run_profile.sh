e4cbb88e1d2b21c37653433383ca4c85a3911d12
blueloveTH <blueloveTH@foxmail.com>
2024-05-18 15:35:15 +0800

    Update run_profile.sh

--- run_profile.sh
+++ run_profile.sh
@@ -1,7 +1,6 @@
 python prebuild.py
 SRC=$(find src/ -name "*.cpp")
-clang++ -pg -O1 -std=c++17 -stdlib=libc++ -frtti -Wfatal-errors -o main $SRC src2/main.cpp -Iinclude
-time ./main benchmarks/fib.py
-mv benchmarks/gmon.out .
+g++ -pg -Og -std=c++17 -frtti -Wfatal-errors -o main $SRC src2/main.cpp -Iinclude
+./main benchmarks/fib.py
 gprof main gmon.out > gprof.txt
 rm gmon.out
