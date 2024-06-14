a8ca70ca74d81d1db562d347975d5cef343a31f8
szdytom <szdytom@qq.com>
2024-06-14 12:24:22 +0800

    more replace

--- src/objects/pyvar.cpp
+++ src/objects/pyvar.cpp
@@ -4,14 +4,14 @@
 
 extern "C" {
 
-bool pkpy_Var__eq__(void *vm_, pkpy_Var a, pkpy_Var b) {
-    auto vm = static_cast<pkpy::VM *>(vm_);
-    return vm->py_eq(*reinterpret_cast<pkpy::PyVar*>(&a), *reinterpret_cast<pkpy::PyVar*>(&b));
+bool pkpy_Var__eq__(void* vm_, pkpy_Var a, pkpy_Var b) {
+    auto vm = (pkpy::VM*)(vm_);
+    return vm->py_eq(*(pkpy::PyVar*)(&a), *(pkpy::PyVar*)(&b));
 }
 
-int64_t pkpy_Var__hash__(void *vm_, pkpy_Var a) {
-    auto vm = static_cast<pkpy::VM *>(vm_);
-    return vm->py_hash(*reinterpret_cast<pkpy::PyVar*>(&a));
+int64_t pkpy_Var__hash__(void* vm_, pkpy_Var a) {
+    auto vm = (pkpy::VM*)(vm_);
+    return vm->py_hash(*(pkpy::PyVar*)(&a));
 }
 
 }
