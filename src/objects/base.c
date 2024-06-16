38040bf9fd1775ceb3a3d1133536345e3902e560
blueloveTH <blueloveTH@foxmail.com>
2024-06-16 21:18:43 +0800

    fix msvc

--- src/objects/base.c
+++ src/objects/base.c
@@ -1,6 +1,6 @@
 #include "pocketpy/objects/base.h"
 
 PyVar pkpy_NULL = {.type=0, .is_ptr=false, .extra=0, ._i64=0};
-PyVar pkpy_OP_CALL = {.type=tp_op_call, .is_ptr=false, .extra=0, ._i64=0};
-PyVar pkpy_OP_YIELD = {.type=tp_op_yield, .is_ptr=false, .extra=0, ._i64=0};
+PyVar pkpy_OP_CALL = {.type=27, .is_ptr=false, .extra=0, ._i64=0};
+PyVar pkpy_OP_YIELD = {.type=28, .is_ptr=false, .extra=0, ._i64=0};
 
