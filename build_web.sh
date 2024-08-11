ca459f6b65c54553dfacd5ca495821d4194ed505
blueloveTH <blueloveth@foxmail.com>
2024-08-11 15:41:07 +0800

    ...

--- build_web.sh
+++ build_web.sh
@@ -7,4 +7,8 @@ mkdir web/lib
 
 SRC=$(find src/ -name "*.c")
 
-emcc $SRC -Iinclude/ -s -Os -sEXPORTED_FUNCTIONS=_py_initialize,_py_finalize,_py_exec,_py_replinput -sEXPORTED_RUNTIME_METHODS=ccall -o web/lib/pocketpy.js
+emcc $SRC -Iinclude/ -s -Os \
+    -sEXPORTED_FUNCTIONS=_py_initialize,_py_exec,_py_finalize,_py_printexc,_py_clearexc \
+    -sEXPORTED_RUNTIME_METHODS=ccall \
+    -sALLOW_MEMORY_GROWTH=1 \
+    -o web/lib/pocketpy.js
