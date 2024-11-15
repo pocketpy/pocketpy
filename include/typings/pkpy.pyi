be6aa75106e79f3dc38966d41651282ea679edfe
blueloveTH <blueloveth@foxmail.com>
2024-11-15 10:44:16 +0800

    Update pkpy.pyi

--- include/typings/pkpy.pyi
+++ include/typings/pkpy.pyi
@@ -1,7 +1,13 @@
 from typing import Self
+from linalg import vec2, vec2i
 
 class TValue[T]:
     def __new__(cls, value: T) -> Self: ...
     
     @property
     def value(self) -> T: ...
+
+TValue_int = TValue[int]
+TValue_float = TValue[float]
+TValue_vec2i = TValue[vec2i]
+TValue_vec2 = TValue[vec2]
