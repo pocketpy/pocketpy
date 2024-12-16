2ef181f0f430b2707831ee0a6284dce6e168cfbd
blueloveTH <blueloveth@foxmail.com>
2024-12-16 19:00:18 +0800

    add benchmarks

---
+++ benchmarks/dumps_loads_json.py
@@ -0,0 +1,24 @@
+import json
+
+data1 = [1, 2, 3] * 100
+data2 = [1.0, 2.0, 3.0] * 100
+data3 = ['abcdefg', 'hijklmn', '_______________1'] * 100
+data4 = [True, False, True] * 100
+data5 = [None, None] * 100
+
+original = {
+    '1': data1,
+    '2': data2,
+    '3': data3,
+    '45': {
+        '4': data4,
+        '5': data5,
+    }
+}
+
+for i in range(10000):
+    encoded = json.dumps(original)
+    decoded = json.loads(encoded)
+    if i == 0:
+        assert original == decoded
+
