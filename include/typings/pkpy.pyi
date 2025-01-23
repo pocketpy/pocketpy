181a9465d62f319b428ccca8ac2b77c18f1f431a
blueloveTH <blueloveth@foxmail.com>
2025-01-23 14:50:36 +0800

    Update pkpy.pyi

--- include/typings/pkpy.pyi
+++ include/typings/pkpy.pyi
@@ -7,10 +7,10 @@ class TValue[T]:
     @property
     def value(self) -> T: ...
 
-TValue_int = TValue[int]
-TValue_float = TValue[float]
-TValue_vec2i = TValue[vec2i]
-TValue_vec2 = TValue[vec2]
+# TValue_int = TValue[int]
+# TValue_float = TValue[float]
+# TValue_vec2i = TValue[vec2i]
+# TValue_vec2 = TValue[vec2]
 
 def memory_usage() -> str:
     """Return a summary of the memory usage."""
