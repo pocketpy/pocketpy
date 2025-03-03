4434bfcf232ee5984b3141eebf12eb9e6e985f4c
blueloveTH <blueloveth@foxmail.com>
2025-03-03 19:30:06 +0800

    use unity build
    Update build_ios.sh
    
    update build scripts

--- build_ios.sh
+++ build_ios.sh
@@ -8,10 +8,7 @@ cd build
 
 FLAGS="-DCMAKE_TOOLCHAIN_FILE=3rd/ios.toolchain.cmake \
     -DDEPLOYMENT_TARGET=13.0 \
-    -DPK_BUILD_STATIC_LIB=ON \
-    -DPK_BUILD_WITH_IPO=OFF \
-    -DPK_BUILD_MODULE_LZ4=ON \
-    -DPK_BUILD_MODULE_LIBHV=ON"
+    -DPK_BUILD_STATIC_LIB=ON"
 
 cmake -B os64 -G Xcode $FLAGS -DPLATFORM=OS64 ..
 cmake --build os64 --config Release
--- include/pocketpy/interpreter/heap.h
+++ include/pocketpy/interpreter/heap.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "pocketpy/objects/object.h"
 #include "pocketpy/interpreter/objectpool.h"
 
