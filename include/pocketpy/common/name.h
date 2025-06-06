6de065899a89f2556b605d7a1021ebeec19cc4ce
blueloveTH <blueloveth@foxmail.com>
2025-06-06 23:12:01 +0800

    Update name.h

--- include/pocketpy/common/name.h
+++ include/pocketpy/common/name.h
@@ -2,6 +2,10 @@
 
 #include "pocketpy/pocketpy.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void pk_names_initialize();
 void pk_names_finalize();
 
@@ -13,3 +17,7 @@ py_Name py_namev(c11_sv name);
 c11_sv py_name2sv(py_Name index);
 py_Name py_name(const char* name);
 const char* py_name2str(py_Name index);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
