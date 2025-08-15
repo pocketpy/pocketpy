839bf0ed1c94adbe14700269d2343979b4302dd6
blueloveTH <blueloveTH@foxmail.com>
2025-08-15 17:22:03 +0800

    Update 98_profiler.py

--- scripts/98_profiler.py
+++ scripts/98_profiler.py
@@ -1,11 +1,11 @@
-from pkpy import *
 import time
 
 def costly_func(n: int):
-    time.sleep(n)
+    time.sleep(n)               # 2s
+    time.sleep(1)               # 1s
 
 x = 1
 y = 2
-costly_func(2)
+costly_func(2)      # 3s
 
-time.sleep(1)
+time.sleep(1)       # 1s
