3be4a35f79cf28e1707b09b3ade8cb27a5c6dae1
blueloveTH <blueloveTH@foxmail.com>
2025-10-20 19:39:01 +0800

    Revert "Update build_web.sh"
    This reverts commit 0c6ac6287d045d013cda6e558215549215ae6a49.

--- build_web.sh
+++ build_web.sh
@@ -9,6 +9,6 @@ SRC=$(find src/ -name "*.c")
 
 emcc $SRC -Iinclude/ -s -Os \
     -sEXPORTED_FUNCTIONS=_py_initialize,_py_exec,_py_finalize,_py_printexc,_py_clearexc \
-    -sEXPORTED_RUNTIME_METHODS="['ccall','cwrap','_emscripten_stack_get_current']"
+    -sEXPORTED_RUNTIME_METHODS=ccall \
     -sALLOW_MEMORY_GROWTH=1 \
     -o web/lib/pocketpy.js
