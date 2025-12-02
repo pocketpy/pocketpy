ab513822b32c61f3ac4c872f13abd91512a71465
blueloveTH <blueloveth@foxmail.com>
2025-12-02 21:37:47 +0800

    update cov

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ python prebuild.py
 
 SRC=$(find src/ -name "*.c")
 
-clang -std=c11 --coverage -O1 -Wfatal-errors -o main src2/main.c $SRC -Iinclude -DPK_ENABLE_OS=1 -DPK_ENABLE_PROFILER=1 -lm -ldl -DNDEBUG
+clang -std=c11 --coverage -O1 -Wfatal-errors -o main src2/main.c $SRC -Iinclude -DPK_ENABLE_OS=1 -lm -ldl -DNDEBUG
 
 python scripts/run_tests.py
 
@@ -16,8 +16,17 @@ fi
 rm -rf .coverage
 mkdir .coverage
 
+# remove .gcno files if the 1st line contains "Source:src/debugger/"
+find . -type f -name "*.gcno" | while read -r file; do
+    first_line=$(head -n 1 "$file")
+    if [[ "$first_line" == *"Source:src/debugger/"* ]]; then
+        echo "Deleting: $file"
+        rm -f "$file"
+    fi
+done
+
 UNITS=$(find ./ -name "*.gcno")
-llvm-cov-15 gcov ${UNITS} -r -s include/ -r -s src/ >> .coverage/coverage.txt
+llvm-cov-17 gcov ${UNITS} -r -s include/ -r -s src/ >> .coverage/coverage.txt
 
 mv *.gcov .coverage
 rm *.gcda
