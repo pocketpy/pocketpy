8d911452bc91577147ff24e96bce10cfc1f4b29f
Phil Chang <lasagna1299@gmail.com>
2026-01-02 21:10:17 +0900

    Add extern specifier for inline functions inside C source files

--- src/objects/object.c
+++ src/objects/object.c
@@ -2,16 +2,16 @@
 #include "pocketpy/pocketpy.h"
 #include <assert.h>
 
-PK_INLINE void* PyObject__userdata(PyObject* self) {
+extern PK_INLINE void* PyObject__userdata(PyObject* self) {
     return self->flex + PK_OBJ_SLOTS_SIZE(self->slots);
 }
 
-PK_INLINE NameDict* PyObject__dict(PyObject* self) {
+extern PK_INLINE NameDict* PyObject__dict(PyObject* self) {
     assert(self->slots == -1);
     return (NameDict*)(self->flex);
 }
 
-PK_INLINE py_TValue* PyObject__slots(PyObject* self) {
+extern PK_INLINE py_TValue* PyObject__slots(PyObject* self) {
     assert(self->slots >= 0);
     return (py_TValue*)(self->flex);
 }
