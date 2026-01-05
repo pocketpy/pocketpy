984c7238727da10ffc803025c381a98da8bfe763
blueloveTH <blueloveTH@foxmail.com>
2026-01-05 19:36:22 +0800

    Update 922_py_compile.py

--- tests/922_py_compile.py
+++ tests/922_py_compile.py
@@ -4,7 +4,12 @@ except ImportError:
     print('os is not enabled, skipping test...')
     exit(0)
 
-assert os.system('./main --compile python/heapq.py heapq1.pyc') == 0
+import sys
+if sys.platform == 'win32':
+    exe_name = 'main.exe'
+else:
+    exe_name = './main'
+assert os.system(f'{exe_name} --compile python/heapq.py heapq1.pyc') == 0
 assert os.path.exists('heapq1.pyc')
 
 import heapq1
