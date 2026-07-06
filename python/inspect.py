1c4e235e9e160d58fc93a8cc5e8e8bdf7cfa08e3
felfoldy <tiboofficial@gmail.com>
2026-07-06 19:41:01 +0200

    Update inspect.signature with annotations

--- python/inspect.py
+++ python/inspect.py
@@ -11,15 +11,20 @@ class Parameter:
 
     empty = _empty
 
-    def __init__(self, name, kind, *default):
+    def __init__(self, name, kind, *default, annotation=None):
         self.name = name
         self.kind = kind
         # pocketpy only allows literal defaults, so use *default as sentinel
         self.default = default[0] if default else _empty
+        self.annotation = _empty if annotation is None else annotation
 
     def __str__(self):
         res = self.name
-        if self.default is not _empty:
+        if self.annotation is not _empty:
+            res += ': ' + self.annotation
+            if self.default is not _empty:
+                res += ' = ' + repr(self.default)
+        elif self.default is not _empty:
             res += '=' + repr(self.default)
         if self.kind == Parameter.VAR_POSITIONAL:
             res = '*' + res
@@ -34,25 +39,34 @@ class Parameter:
 class Signature:
     empty = _empty
 
-    def __init__(self, parameters):
+    def __init__(self, parameters, return_annotation=None):
         self.parameters = {p.name: p for p in parameters}
+        self.return_annotation = _empty if return_annotation is None else return_annotation
 
     def __str__(self):
-        return '(' + ', '.join([str(p) for p in self.parameters.values()]) + ')'
+        res = '(' + ', '.join([str(p) for p in self.parameters.values()]) + ')'
+        if self.return_annotation is not _empty:
+            res += ' -> ' + self.return_annotation
+        return res
 
     def __repr__(self):
         return '<Signature ' + str(self) + '>'
 
 
-def _make_params(func):
-    return [Parameter(*entry) for entry in _signature_data(func)]
+def _from_function(func, drop_first):
+    annotations = func.__annotations__
+    params = [Parameter(*entry, annotation=annotations.get(entry[0]))
+              for entry in _signature_data(func)]
+    if drop_first:
+        params = params[1:]
+    return Signature(params, annotations.get('return'))
 
 
 def signature(obj):
     if not callable(obj):
         raise TypeError(repr(obj) + ' is not a callable object')
     if isinstance(obj, type):
-        return Signature(_make_params(obj.__init__)[1:])
+        return _from_function(obj.__init__, True)
     if hasattr(obj, '__func__'):
-        return Signature(_make_params(obj.__func__)[1:])
-    return Signature(_make_params(obj))
+        return _from_function(obj.__func__, True)
+    return _from_function(obj, False)
