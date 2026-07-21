f2b6d90a7214e56d61813dcc96de2cc8ce36c5f5
blueloveTH <blueloveth@foxmail.com>
2026-07-21 14:30:09 +0800

    Update mpack1.h

--- 3rd/msgpack/include/mpack1.h
+++ 3rd/msgpack/include/mpack1.h
@@ -3,6 +3,10 @@
 #include "mpack.h"
 #include "pocketpy.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef bool (*is_mpack_ext_t)(py_Type type);
 typedef bool (*py_to_mpack_ext_t)(py_Ref object, mpack_writer_t* writer);
 typedef bool (*mpack_to_py_ext_t)(py_StackRef tmp, mpack_node_t node);
@@ -10,3 +14,7 @@ typedef bool (*mpack_to_py_ext_t)(py_StackRef tmp, mpack_node_t node);
 void mpack_config_exttype_callbacks(is_mpack_ext_t is_ext,
                                     py_to_mpack_ext_t to_mpack,
                                     mpack_to_py_ext_t to_py);
+
+#ifdef __cplusplus
+}
+#endif
