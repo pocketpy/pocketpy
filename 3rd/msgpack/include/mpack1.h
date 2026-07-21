08fce73bbb3a6a3c692a5018c4d3b73cd17ebb6d
blueloveTH <blueloveth@foxmail.com>
2026-07-21 14:27:18 +0800

    Update mpack1.h

--- 3rd/msgpack/include/mpack1.h
+++ 3rd/msgpack/include/mpack1.h
@@ -5,7 +5,7 @@
 
 typedef bool (*is_mpack_ext_t)(py_Type type);
 typedef bool (*py_to_mpack_ext_t)(py_Ref object, mpack_writer_t* writer);
-typedef bool (*mpack_to_py_ext_t)(mpack_node_t node);
+typedef bool (*mpack_to_py_ext_t)(py_StackRef tmp, mpack_node_t node);
 
 void mpack_config_exttype_callbacks(is_mpack_ext_t is_ext,
                                     py_to_mpack_ext_t to_mpack,
